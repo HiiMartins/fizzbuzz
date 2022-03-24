47f6af8771516b1ec16fc4142dd5c90b8284c702
HiiMartins <higor.dsm10@gmail.com>
2022-03-24 01:00:43 -0300

    implemntaçao dos casos de teste de build/1

--- test/fizz_buzz_test.exs
+++ test/fizz_buzz_test.exs
@@ -1,8 +1,15 @@
 defmodule FizzBuzzTest do
   use ExUnit.Case
-  doctest FizzBuzz
 
-  test "read the file" do
-    assert FizzBuzz.build("list_numbers.txt") == "[1,2,5,9,10,13,15]"
+  describe "build/1" do
+    test "when a valid file is provided, returns the converted list" do
+      expected_response = {:ok, [1, 2, :buzz, :fizz, :buzz, 13, :fizzbuzz]}
+      assert FizzBuzz.build("list_numbers.txt") == expected_response
+    end
+
+    test "when an invalid file is provided, returns an error" do
+      expected_response = {:error, "Error reading the file: enoent"}
+      assert FizzBuzz.build("invalid.txt") == expected_response
+    end
   end
 end
